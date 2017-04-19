5037b89fb222bb739dcf1971bfe82d8e1d7c2c45
sist <sist@localhost>
2017-04-19 16:09:06 +0900

    1

---
+++ MusicProject/src/main/java/com/aclass/mgr/MusicVO.java
@@ -0,0 +1,41 @@
+package com.aclass.mgr;
+
+public class MusicVO {
+	private String poster;
+	private String title;
+	private String artist;
+	private String albumname;
+	private int like;
+	public String getPoster() {
+		return poster;
+	}
+	public void setPoster(String poster) {
+		this.poster = poster;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getArtist() {
+		return artist;
+	}
+	public void setArtist(String artist) {
+		this.artist = artist;
+	}
+	public String getAlbumname() {
+		return albumname;
+	}
+	public void setAlbumname(String albumname) {
+		this.albumname = albumname;
+	}
+	public int getLike() {
+		return like;
+	}
+	public void setLike(int like) {
+		this.like = like;
+	}
+	
+	
+}
