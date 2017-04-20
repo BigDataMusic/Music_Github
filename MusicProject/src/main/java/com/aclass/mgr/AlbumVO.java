3c68b311f0ac2ad9d11a0705a54b223b306445cf
sist <sist@ns4>
2017-04-20 19:21:20 +0900

    album

---
+++ MusicProject/src/main/java/com/aclass/mgr/AlbumVO.java
@@ -0,0 +1,102 @@
+package com.aclass.mgr;
+import java.util.*;
+public class AlbumVO {
+	private int alNo;
+	private int alRank;
+	private String alTitle;
+	private String alPoster;
+	private String alArtist;
+	private String alRegdate;
+	private String alGenre;
+	private String saleCo;
+	private String entertainment;
+	private double alPoint;
+	private int alLike;
+	private String alInfo;
+	List<MusicVO> mList= new ArrayList<MusicVO>();
+	
+	
+	public int getAlRank() {
+		return alRank;
+	}
+	public void setAlRank(int alRank) {
+		this.alRank = alRank;
+	}
+	public int getAlNo() {
+		return alNo;
+	}
+	public void setAlNo(int alNo) {
+		this.alNo = alNo;
+	}
+	public double getAlPoint() {
+		return alPoint;
+	}
+	public void setAlPoint(double alPoint) {
+		this.alPoint = alPoint;
+	}
+	public int getAlLike() {
+		return alLike;
+	}
+	public void setAlLike(int alLike) {
+		this.alLike = alLike;
+	}
+	public String getAlInfo() {
+		return alInfo;
+	}
+	public void setAlInfo(String alInfo) {
+		this.alInfo = alInfo;
+	}
+	public String getAlTitle() {
+		return alTitle;
+	}
+	public void setAlTitle(String alTitle) {
+		this.alTitle = alTitle;
+	}
+	public String getAlPoster() {
+		return alPoster;
+	}
+	public void setAlPoster(String alPoster) {
+		this.alPoster = alPoster;
+	}
+	public String getAlArtist() {
+		return alArtist;
+	}
+	public void setAlArtist(String alArtist) {
+		this.alArtist = alArtist;
+	}
+	public String getAlRegdate() {
+		return alRegdate;
+	}
+	public void setAlRegdate(String alRegdate) {
+		this.alRegdate = alRegdate;
+	}
+	public String getAlGenre() {
+		return alGenre;
+	}
+	public void setAlGenre(String alGenre) {
+		this.alGenre = alGenre;
+	}
+	public String getSaleCo() {
+		return saleCo;
+	}
+	public void setSaleCo(String saleCo) {
+		this.saleCo = saleCo;
+	}
+	public String getEntertainment() {
+		return entertainment;
+	}
+	public void setEntertainment(String entertainment) {
+		this.entertainment = entertainment;
+	}
+	public List<MusicVO> getmList() {
+		return mList;
+	}
+	public void setmList(List<MusicVO> mList) {
+		this.mList = mList;
+	}
+	
+	
+	
+	
+	
+}
