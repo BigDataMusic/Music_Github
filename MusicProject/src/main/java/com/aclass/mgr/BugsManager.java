6b5d3dd555f572626abcc345a939db05d88e5333
soo <skysg0409@gmail.com>
2017-04-20 18:55:15 +0900

    bugs

--- MusicProject/src/main/java/com/aclass/mgr/BugsManager.java
+++ MusicProject/src/main/java/com/aclass/mgr/BugsManager.java
@@ -13,42 +13,47 @@ public class BugsManager {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		BugsManager bm=new BugsManager();
+		BugsManager bm = new BugsManager();
 		bm.bugsRankData();
 	}
-	
-	public List<MusicVO> bugsRankData()
-	   {
-		    List<MusicVO> bList= new ArrayList<MusicVO>();
-		    try{
-		        Document doc=Jsoup.connect("http://music.bugs.co.kr/chart/track/realtime/total").get();
-		        Elements tElem=doc.select("td input");
-		        Elements pElem=doc.select("td a img");
-		        Elements aElem=doc.select("td p a");
-		        Elements iElem=doc.select("div.ranking p span");
-		        
-		        for(int i=0;i<100;i++) {
-		        	 Element t=tElem.get(i);
-		        	 String title=t.attr("title");
-		        	 Element pos=pElem.get(i);
-		        	 String poster=pos.attr("src");
-		        	 Element a=aElem.get(i);
-		        	 Element in=iElem.get(i);
-		        	 
-		        	 MusicVO vo=new MusicVO();
-		        	 vo.setRank(i+1);
-		        	 vo.setTitle(title.trim());
-		        	 vo.setPoster(poster.trim());
-		        	 vo.setArtist(a.text().trim());
-		        	 vo.setIncrement(in.text());
-		        	 //list.add(vo);
-		        	 System.out.println(vo.getRank()+"위 "+vo.getTitle()+" - "+vo.getArtist()+" - "+vo.getPoster());
-		         }
-		        
-		    }catch(Exception ex)  {
-		    	System.out.println("bugsRankData "+ex.getMessage());
-		     }
-		    return bList;
-	   }
-}
 
+	public List<MusicVO> bugsRankData() {
+		List<MusicVO> bList = new ArrayList<MusicVO>();
+		try {
+			Document doc = Jsoup.connect("http://music.bugs.co.kr/chart/track/realtime/total").get();
+			Elements tElem = doc.select("td input");
+			Elements pElem = doc.select("td a img");
+			Elements artElem = doc.select("td.left p.artist a:eq(0)");
+			Elements alElem = doc.select("td.left a.album");
+			Elements iElem = doc.select("div.ranking p");
+			
+			for (int i = 0; i < 100; i++) {
+				Element t = tElem.get(i);
+				String title = t.attr("title");
+				Element pos = pElem.get(i);
+				String poster = pos.attr("src");
+				Element art = artElem.get(i);
+				Element al = alElem.get(i);
+				Element in = iElem.get(i);
+
+				MusicVO vo = new MusicVO();
+				vo.setRank(i + 1);
+				vo.setTitle(title.trim());
+				vo.setPoster(poster.trim());
+				vo.setArtist(art.text().trim());
+				vo.setAlbumname(al.text().trim());
+				vo.setIncrement(in.text());
+				// list.add(vo);
+				
+				System.out.println(vo.getRank()+"위 "+vo.getTitle()+" - "+vo.getArtist());//+" - "+vo.getAlbumname()+" - "
+				System.out.println("   "+vo.getPoster());
+				System.out.println("   "+vo.getIncrement());
+				System.out.println("   "+vo.getAlbumname());
+			}
+
+		} catch (Exception ex) {
+			System.out.println("bugsRankData " + ex.getMessage());
+		}
+		return bList;
+	}
+}
