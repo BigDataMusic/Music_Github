315bd7289e066ca106e5b9a53ce93db7ab6c31e9
sist <sist@ns4>
2017-05-18 15:42:04 +0900

    main controller, nav 수정

--- MusicProject/src/main/java/com/aclass/music/MainController.java
+++ MusicProject/src/main/java/com/aclass/music/MainController.java
@@ -83,7 +83,7 @@ public class MainController{
 	private SparkWeatherManager sparkweather;
 
 	@RequestMapping("main.do")
-	public String main_page(String data,Model model)
+	public String main_page(String feel, String data,Model model)
 	{
 		// 음악인차트
 		/*List<MusicVO> bList = bmgr.bugsRankData();
@@ -120,6 +120,13 @@ public class MainController{
     	model.addAttribute("niList", niList);
     	model.addAttribute("nList", nList);
 		model.addAttribute("bList", bList);
+		
+		// nav추천
+    	if(feel==null)
+    	feel="봄";
+		List<SongVO> list=recomdao.songRecommandData(feel);
+		model.addAttribute("list", list);
+		
 		return "main";
 	}
 	@RequestMapping("content.do")
