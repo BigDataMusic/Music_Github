3ff9a5901925a7b6ae11cf3b72ef6c82f2498fcf
soo <skysg0409@gmail.com>
2017-05-19 17:24:48 +0900

    spark

--- MusicProject/src/main/java/com/aclass/review/naver/RManager.java
+++ MusicProject/src/main/java/com/aclass/review/naver/RManager.java
@@ -43,7 +43,7 @@ public void rGraph(String song)
 		   rc.voidEval("library(stringr)");
 		   
 		   rc.voidEval("png(\"/home/sist/sparkDev/.metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/MusicProject/resources/images/wordcloud.png\")");
-		   rc.voidEval("png(\"/home/sist/bigdataDev/.metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/MusicProject/resources/images/wordcloud.png\")");
+		   rc.voidEval("png(\"/home/sist/sparkDev/.metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/MusicProject/resources/images/wordcloud.png\")");
 		
 		   rc.voidEval("mongo<-mongoDbConnect(\"project3\",\"211.238.142.38\",27017)");
 
