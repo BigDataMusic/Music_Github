e59c577efad774f4c4dcc9a7bb1801ab5625b0a6
soo <skysg0409@gmail.com>
2017-05-22 17:23:01 +0900

    sjsj

--- MusicProject/src/main/java/com/aclass/mgr/VideoManager.java
+++ MusicProject/src/main/java/com/aclass/mgr/VideoManager.java
@@ -11,17 +11,18 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class VideoManager {
-  /* public static void main(String[] args) {
+  public static void main(String[] args) {
       // TODO Auto-generated method stub
       VideoManager vm= new VideoManager();
-      vm.getVideo("뮤지컬 매디슨 카운티의 다리");
-   }*/
+      vm.getVideo("이 지금");
+   }
    public List<VideoVO> getVideo(String title){
       List<VideoVO> list = new ArrayList<VideoVO>();
       try {
          String ucode=URLEncoder.encode(title,"UTF-8");
+         	//System.out.println(ucode);
          //ucode="%EB%AE%A4%EC%A7%80%EC%BB%AC+%EB%A7%A4%EB%94%94%EC%8A%A8+%EC%9A%B4%ED%8B%B0%EC%9D%98+%EB%8B%A4%EB%A6%AC";
-         Document doc = Jsoup.connect("https://www.youtube.com/results?search_query="+ucode).get();
+         Document doc = Jsoup.connect("https://www.youtube.com/results?search_query="+title).get();
     	  //Document doc = Jsoup.connect("https://www.youtube.com/results?search_query=%EB%93%9C%EB%A6%BC%EA%B1%B8%EC%A6%88(DREAMGIRLS)%20%EC%B5%9C%EC%B4%88%20%EB%82%B4%ED%95%9C").get();
          //System.out.println(title);
          Element video=doc.select("ol.item-section").first();
